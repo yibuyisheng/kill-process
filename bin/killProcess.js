99925c01070fa17926e15008540dd9cea2cb0058
yibuyisheng <yibuyisheng@163.com>
2015-04-19 15:59:39 +0800

    fix

--- bin/killProcess.js
+++ bin/killProcess.js
@@ -5,7 +5,7 @@ var utils = require('utilities');
 
 if (process.argv.length > 3) {
     console.log('too many arguments. only support killing process by port currently');
-} else if (utils.isNumber(process.argv[2]) || process.argv[2] <= 0) {
+} else if (utils.base.isNumber(process.argv[2]) || process.argv[2] <= 0) {
     console.log('the third argument is not a port number');
 } else {
     killProcess(process.argv[2]);
